6ff951111487aee1c03b0c58a5bbeb8a08c66b94
Ryan Driscoll <driscollrp@gmail.com>
2017-01-10 15:45:04 -0600

    added bootstrap, navbar visible

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,14 +5,14 @@ import WhoAmI from '../components/WhoAmI'
 export default (props) => {
   return (
     <div>
-      <div className="navbar navbar-fixed-top" role="navigation">
+      <div className="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div className="navbar-header">
           <a className="navbar-brand" href="/"><img src="/share.png" alt="share logo"></img><span>Dai.ly</span></a>
         </div>
         <div className="collapse navbar-collapse">
           <ul className="nav navbar-nav">
             <li>
-              {user ? <WhoAmI/> : <Login/>}
+              {props.user ? <WhoAmI/> : <a href="/login">Login</a>}
             </li>
             <li><a href="/about">About</a></li>
             <li><a href="/contact">Contact</a></li>
--- app/main.jsx
+++ app/main.jsx
@@ -5,7 +5,6 @@ import {render} from 'react-dom'
 import {connect, Provider} from 'react-redux'
 
 import store from './store'
-import Jokes from './components/Jokes'
 import Login from './components/Login'
 import WhoAmI from './components/WhoAmI'
 import Navbar from './components/Navbar'
@@ -24,8 +23,7 @@ render (
   <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={ExampleApp}>
-        <IndexRedirect to="/jokes" />
-        <Route path="/jokes" component={Jokes} />
+        {/*<IndexRedirect to="/products" />*/}
       </Route>
     </Router>
   </Provider>,
