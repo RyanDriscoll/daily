4735767e47535f1c2c7a80775b2d84986f14faa9
spencewine <sdgoodwine@gmail.com>
2017-01-11 15:57:18 -0600

    7_ProductViews after conflict resolution

--- app/reducers/index.jsx
+++ app/reducers/index.jsx
@@ -1,11 +1,10 @@
 import { combineReducers } from 'redux'
-import user from 'APP/app/reducers/user';
 import products from 'APP/app/reducers/products'
 import userProfile from 'APP/app/reducers/userProfile';
 
 
 const rootReducer = combineReducers({
-  auth: require('./auth').default, user, products, userProfile
+  auth: require('./auth').default, products, userProfile
 })
 
 export default rootReducer
