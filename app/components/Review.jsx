ecdada271630b743fab4416eba1c7bd7af4e3190
Rebekah Klemm <rebekah_klemm@yahoo.com>
2017-01-12 15:27:56 -0600

    star rating in progress

--- app/components/Review.jsx
+++ app/components/Review.jsx
@@ -1,17 +1,27 @@
 import React, {Component} from 'react';
+// import rating from './stars/star';
+import StarRating from 'react-bootstrap-star-rating';
 
 
 export default function Review(props) {
     let reviews = props.review;
 
     console.log('props.review in review', props.review)
+
     return(
-        <div>
+        <div className='col-md-10'>
             {reviews.map( singleReview => {
                     return (
                         <div key={singleReview.id}>
                             <p>id: {singleReview.id},</p>
                             <p>stars: {singleReview.stars},</p>
+
+                            <StarRating
+                                className='rating'
+                                defaultValue={5}
+                                min={0}
+                                max={10}
+                                step={0.5} />
                             <p>text: {singleReview.text},</p>
                             <p>reservation_id: {singleReview.reservation_id}</p>
                         </div>
@@ -22,6 +32,4 @@ export default function Review(props) {
     )
 
 }
-
-
 
